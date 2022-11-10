2a8ec3effa772c8b8303b67cdc0de2837e8eac12
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-11-10 17:46:16 +0000

    Add project files.

---
+++ NullReferences/Guy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullReferences
+{
+#nullable enable
+    class Guy
+    {
+        public string Name { get; private set; }
+        public int Age { get; set; }
+        public override string ToString() => $"a {Age}-year old named {Name}";
+
+        public Guy(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+}
---
+++ NullReferences/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NullReferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy guy;
+            guy = new Guy(12,"flob");
+            Console.WriteLine("guy.Name is {0} letters long", guy.Name.Length);
+
+        }
+    }
+}
